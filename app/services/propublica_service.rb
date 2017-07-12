546f5a3ad06a228a4a8c4ade65a42cc838f156a4
Chris Smith <42@chrisryansmith.com>
2017-07-12 10:50:38 -0600

    sort search results by seniority DESC

--- app/services/propublica_service.rb
+++ app/services/propublica_service.rb
@@ -13,7 +13,7 @@ class PropublicaService
       req.url "/congress/v1/members/house/#{attrs[:state]}/current.json"
       req.headers["X-API-Key"] = ENV['propublica_key']
     end
-    JSON.parse(response.body)["results"]
+    JSON.parse(response.body)["results"].sort { |x,y| y["seniority"].to_i <=> x["seniority"].to_i }
   end
 
   private
