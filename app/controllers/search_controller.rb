4eb758d1d4016a9aeae4c5793e2f593b2cb60248
Chris Smith <42@chrisryansmith.com>
2017-07-12 10:08:04 -0600

    refactor into PropublicaService object

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,5 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new("https://api.propublica.org/congress/v1/members/house/#{params[:state]}/current.json")
-    response = conn.get do |req|
-      req.headers["X-API-Key"] = ENV['propublica_key']
-    end
-    @results = JSON.parse(response.body)["results"]
+    @results = PropublicaService.search_house_by_state(state: params[:state])
   end
 end
---
+++ app/services/propublica_service.rb
@@ -0,0 +1,22 @@
+class PropublicaService
+  def initialize(attrs)
+    @attrs = attrs
+    @conn = Faraday.new("https://api.propublica.org")
+  end
+
+  def self.search_house_by_state(attrs)
+    new(attrs).search_house_by_state
+  end
+
+  def search_house_by_state
+    response = conn.get do |req|
+      req.url "/congress/v1/members/house/#{attrs[:state]}/current.json"
+      req.headers["X-API-Key"] = ENV['propublica_key']
+    end
+    JSON.parse(response.body)["results"]
+  end
+
+  private
+    attr_reader :attrs, :conn
+
+end
