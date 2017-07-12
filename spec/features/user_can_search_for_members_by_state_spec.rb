a318d3abf56ae7749a8128eb3c5d71c306fe625d
Chris Smith <42@chrisryansmith.com>
2017-07-12 10:58:58 -0600

    add VCRs for API testing

--- spec/features/user_can_search_for_members_by_state_spec.rb
+++ spec/features/user_can_search_for_members_by_state_spec.rb
@@ -9,20 +9,22 @@ require 'rails_helper'
 
 RSpec.feature 'User can search by state' do
   it 'should return the members of that state\'s congressional delegation' do
-    visit root_path
-    select "Colorado"
-    click_on "Locate Members of the House"
+    VCR.use_cassette("Propublica.search_house_by_state") do
+      visit root_path
+      select "Colorado"
+      click_on "Locate Members of the House"
 
-    expect(current_path).to eq search_path
-    expect(page).to have_content "7 Results"
-    expect(page).to have_css('.member', count: 7)
-    # And they should be ordered by seniority from most to least
-    # And I should see a name, role, party, and district for each member
-    members = page.all(".member")
-    expect(members.count).to eq 7
-    expect(members[0]).to have_css('.name')
-    expect(members[0]).to have_css('.role')
-    expect(members[0]).to have_css('.party')
-    expect(members[0]).to have_css('.district')
+      expect(current_path).to eq search_path
+      expect(page).to have_content "7 Results"
+      expect(page).to have_css('.member', count: 7)
+      # And they should be ordered by seniority from most to least
+      # And I should see a name, role, party, and district for each member
+      members = page.all(".member")
+      expect(members.count).to eq 7
+      expect(members[0]).to have_css('.name')
+      expect(members[0]).to have_css('.role')
+      expect(members[0]).to have_css('.party')
+      expect(members[0]).to have_css('.district')
+    end
   end
 end
